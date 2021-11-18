c87c18a31f7372cab4cb5ab96961ea85c98eae5b
Jackson <jack0702@hotmail.com.br>
2021-11-18 13:17:46 -0300

    backend praticamente pronto

--- controllers/ControllerL.js
+++ controllers/ControllerL.js
@@ -4,28 +4,55 @@ const servicos = new servicosList;
 class ControllerList {
     getList = async (req,res)=>{
         const lista = await servicos.findAll();
-        res.send(lista);
+        if (!lista){res.status(404).send(" Lista de tarefas não encontrada"); return;};
+        res.status(200).send(lista);
     };
     getTarefa = async (req,res)=>{
         const id = req.params.id;
+        if(id <= 0) {
+            res.status(404).send({
+                error: 'Tarefa não encontrada!'
+            });
+            return;
+        };
         const tarefa = await servicos.find(id);
-        res.send(tarefa);
+        res.status(200).send(tarefa);
     };
     postTarefa = async (req,res)=>{
         const novaTarefa = req.body;
+        if (!novaTarefa.titulo || !novaTarefa.descricao || !novaTarefa.prioridade || !novaTarefa.prazo || !novaTarefa.status){
+            res.status(400).send({message: 'Preencha todos os campos'});
+        return;
+        };
         await servicos.inserirTarefa(novaTarefa);
-        res.send("tarefa adicionada");
+        res.status(201).send("tarefa adicionada");
     };
     deletarTarefa = async (req,res)=>{
         const id = req.params.id;
+        if(id <= 0) {
+            res.status(404).send({
+                error: 'Tarefa não encontrada!'
+            });
+            return;
+        };
         await servicos.delete(id);
-        res.send("tarefa excluída!");
+        res.status(200).send("tarefa excluída!");
     };
     putTarefa = async (req,res)=>{
         const id = req.params.id;
+        if(id <= 0) {
+            res.status(404).send({
+                error: 'Tarefa não encontrada!'
+            });
+            return;
+        };
         const newTarefa = req.body;
+        if (!newTarefa.titulo || !newTarefa.descricao || !newTarefa.prioridade || !newTarefa.prazo || !newTarefa.status){
+            res.status(400).send({message: 'Preencha todos os campos'});
+        return;
+        };
         await servicos.atualizarTarefa(id, newTarefa);
-        res.send("tarefa atualizada");
+        res.status(200).send("tarefa atualizada");
     };
 };
 module.exports = ControllerList;
