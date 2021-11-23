64bc559dff4429bee188ebd983fdad6ad0141a65
Jackson <jack0702@hotmail.com.br>
2021-11-22 21:38:38 -0300

    conectado

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()};
 const express = require("express");
 const app = express();
 app.use(express.json());
@@ -5,11 +7,16 @@ const cors = require("cors");
 app.use(cors());
 
 const conN = require("./Conn/conn");
-conN();
+const db_url = process.env.DB_URL;
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
+const db_data = process.env.DB_DATA;
+
+conN(db_url,db_user,db_pass,db_data);
 
 const routeS = require("./rotas/routes");
 app.use("/", routeS);
 
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log(" O Servidor está rodando na porta 3001");
 });
